17b8c89a3c83d060575550f42b062e74b2c14701
Philipp Regenauer <p.regenauer@gmx.de>
2019-12-07 12:49:42 +0100

    Initial upload of contents

---
+++ src/modules/Test.py
@@ -0,0 +1,7 @@
+print("my first python module was successfully loaded")
+
+def welt():
+    print("Hallo Welt!")
+    
+def mars():
+    print("Hallo Mars!")
---
+++ src/modules/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ["Test"]
+
+from . import Test
